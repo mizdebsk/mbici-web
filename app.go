fb9b71186e9b7d545131e29e56fb0a21d51a7f7b
Mikolaj Izdebski <mizdebsk@redhat.com>
2023-02-11 11:52:56 +0100

    Add SVG chart

--- app.go
+++ app.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
+	//"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type Artifact struct {
@@ -38,7 +39,8 @@ type Task struct {
 	Handler      string      `xml:"handler"`
 	Dependencies []string    `xml:"dependency"`
 	Parameters   []Parameter `xml:"parameter"`
-	Result       Result
+	Result       *Result
+	Bucket       string
 }
 
 type Workflow struct {
@@ -47,30 +49,112 @@ type Workflow struct {
 	Results []Result `xml:"result"`
 }
 
+type Bucket struct {
+	Tasks     []Task
+	NTasks    int
+	NComplete int
+	NFailed   int
+	NPending  int
+	State     string
+}
+
+type Data struct {
+	Workflow  Workflow
+	SCM       Bucket
+	SRPM      Bucket
+	JPB       Bucket
+	Bootstrap Bucket
+	Rebuild   Bucket
+	Validate  Bucket
+}
+
+func count(b *Bucket) {
+	for _, task := range b.Tasks {
+		b.NTasks += 1
+		if task.Result == nil {
+			b.NPending += 1
+		} else if task.Result.Outcome == "SUCCESS" {
+			b.NComplete += 1
+		} else {
+			b.NFailed += 1
+		}
+	}
+	if b.NFailed > 0 {
+		b.State = "Failed"
+	} else if b.NPending > 0 {
+		b.State = "Pending"
+	} else {
+		b.State = "Succeeded"
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	url := os.Getenv("WORKFLOW_XML")
-	if url == "" {
-		url = "https://mbi-artifacts.s3.eu-central-1.amazonaws.com/1653bbcc-1ae3-4eaa-949a-239a24cf8de9/workflow.xml"
-	}
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		log.Fatal(fmt.Errorf("HTTP GET failed: %s", resp.Status))
-	}
-	resps, err := ioutil.ReadAll(resp.Body)
+	// url := os.Getenv("WORKFLOW_XML")
+	// if url == "" {
+	// 	url = "https://mbi-artifacts.s3.eu-central-1.amazonaws.com/1653bbcc-1ae3-4eaa-949a-239a24cf8de9/workflow.xml"
+	// }
+	// resp, err := http.Get(url)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if resp.StatusCode != http.StatusOK {
+	// 	resp.Body.Close()
+	// 	log.Fatal(fmt.Errorf("HTTP GET failed: %s", resp.Status))
+	// }
+	// bytes, err := ioutil.ReadAll(resp.Body)
+
+	//bytes, err := os.ReadFile("/home/kojan/git/mbici-web/workflow.xml")
+	//bytes, err := os.ReadFile("/home/kojan/git/mbici-local/test/workflow.xml")
+	bytes, err := os.ReadFile("/home/kojan/git/mbici-fedora-pr/test/workflow.xml")
 	if err != nil {
 		log.Fatal(err)
 	}
 	var workflow Workflow
-	if err := xml.Unmarshal(resps, &workflow); err != nil {
-		resp.Body.Close()
+	if err := xml.Unmarshal(bytes, &workflow); err != nil {
+		//resp.Body.Close()
 		log.Fatal(err)
 	}
+
+	for j, result := range workflow.Results {
+		for i, task := range workflow.Tasks {
+			if result.TaskId == task.Id {
+				workflow.Tasks[i].Result = &workflow.Results[j]
+			}
+		}
+	}
+
+	var data Data
+	data.Workflow = workflow
+
+	for _, task := range workflow.Tasks {
+		if strings.HasSuffix(task.Id, "-checkout") {
+			data.SCM.Tasks = append(data.SCM.Tasks, task)
+		} else if strings.HasSuffix(task.Id, "-srpm") || task.Id == "platform" {
+			data.SRPM.Tasks = append(data.SRPM.Tasks, task)
+		} else if strings.HasSuffix(task.Id, "-rpm") || strings.HasSuffix(task.Id, "-repo") {
+			if strings.HasSuffix(task.Id, "-b2-rpm") || task.Id == "b2-repo" {
+				data.JPB.Tasks = append(data.JPB.Tasks, task)
+			} else if strings.HasSuffix(task.Id, "-b3-rpm") || task.Id == "b3-repo" {
+				data.Bootstrap.Tasks = append(data.Bootstrap.Tasks, task)
+			} else {
+				data.Rebuild.Tasks = append(data.Rebuild.Tasks, task)
+			}
+		} else if strings.HasSuffix(task.Id, "-validate") {
+			data.Validate.Tasks = append(data.Validate.Tasks, task)
+		} else {
+			log.Fatal(fmt.Sprintf("Unmatched task ID: %s", task.Id))
+		}
+	}
+
+	count(&data.SCM)
+	count(&data.SRPM)
+	count(&data.JPB)
+	count(&data.Bootstrap)
+	count(&data.Rebuild)
+	count(&data.Validate)
+
 	w.Header().Add("Content-Type", "text/html")
-	err = Template.Execute(w, workflow)
+	err = Template.Execute(w, data)
 	if err != nil {
 		fmt.Println(err)
 	}
--- template.go
+++ template.go
@@ -38,29 +38,215 @@ var Template = template.Must(template.New("tpl").Parse(`
     </nav>
     <main class='container'>
 
+<svg
+   width="1270"
+   height="220"
+   viewBox="0 0 336.02082 58.208335"
+   version="1.1"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <style>
+    path {
+	fill:none;stroke:#000000;stroke-width:0.264583px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1;
+    }
+    .state-Succeeded path {
+	fill: lightgreen
+    }
+    .state-Failed path {
+	fill: red
+    }
+    .left {
+        font-size:3.52778px;
+	stroke-width:0.264583;
+	text-align:start;
+	text-anchor:start;
+    }
+    .right {
+        font-size:3.52778px;
+	stroke-width:0.264583;
+	text-align:end;
+	text-anchor:end;
+    }
+    .caption {
+	font-size:7.76111px;
+	line-height:1.25;
+	font-family:sans-serif;
+	text-align:center;
+	text-anchor:middle;
+	stroke-width:0.264583;
+	font-weight:bold;
+	font-size:7.76111px;
+	stroke-width:0.264583;
+    }
+    .state {
+	font-size:4.93889px;
+	line-height:1.25;
+	font-family:sans-serif;
+	text-align:center;
+	text-anchor:middle;
+	stroke-width:0.264583;
+    }
+  </style>
+
+  <g>
+    <g class="state-{{ .SCM.State }}">
+      <path d="M 2.6458333,55.562499 H 55.562499 v -31.75 H 2.6458333 Z"/>
+      <path d="M 2.6458333,23.812499 V 2.6458333 H 55.562499 l 5.291667,10.5833327 -5.291667,10.583333 z"/>
+    </g>
+    <g class="state-{{ .SRPM.State }}">
+      <path d="M 58.208333,55.562499 H 111.125 v -31.75 H 58.208333 Z"/>
+      <path d="M 58.208333,2.6458333 63.499999,13.229166 58.208333,23.812499 H 111.125 L 116.41666,13.229166 111.125,2.6458333 Z"/>
+    </g>
+    <g class="state-{{ .JPB.State }}">
+      <path d="m 113.77083,55.562499 h 52.91667 v -31.75 h -52.91667 z"/>
+      <path d="m 113.77083,2.6458333 5.29167,10.5833327 -5.29167,10.583333 H 166.6875 L 171.97916,13.229166 166.6875,2.6458333 Z"/>
+    </g>
+    <g class="state-{{ .Bootstrap.State }}">
+      <path d="M 169.33333,55.562499 H 222.25 v -31.75 h -52.91667 z"/>
+      <path d="M 169.33333,2.6458333 174.625,13.229166 169.33333,23.812499 H 222.25 L 227.54166,13.229166 222.25,2.6458333 Z"/>
+    </g>
+    <g class="state-{{ .Rebuild.State }}">
+      <path d="m 224.89583,55.562499 h 52.91667 v -31.75 h -52.91667 z"/>
+      <path d="m 224.89583,2.6458333 5.29167,10.5833327 -5.29167,10.583333 H 277.8125 L 283.10416,13.229166 277.8125,2.6458333 Z"/>
+    </g>
+    <g class="state-{{ .Validate.State }}">
+      <path d="M 280.45833,55.562499 H 333.375 v -31.75 h -52.91667 z"/>
+      <path d="M 280.45833,23.812499 285.75,13.229166 280.45833,2.6458333 H 333.375 V 23.812499 Z"/>
+    </g>
+
+    <g class="caption">
+      <text x="29.104166" y="15.875">SCM</text>
+      <text x="87.3125" y="15.875">SRPM</text>
+      <text x="142.875" y="15.875">JPB</text>
+      <text x="198.4375" y="15.875">Bootstrap</text>
+      <text x="254" y="15.875">Rebuild</text>
+      <text x="309.5625" y="15.875">Validate</text>
+    </g>
+
+    <g class="state">
+      <text x="29.104166" y="31.749998">{{ .SCM.State }}</text>
+      <text x="84.666664" y="31.749998">{{ .SRPM.State }}</text>
+      <text x="140.22916" y="31.749998">{{ .JPB.State }}</text>
+      <text x="195.79166" y="31.749998">{{ .Bootstrap.State }}</text>
+      <text x="251.35416" y="31.749998">{{ .Rebuild.State }}</text>
+      <text x="306.91666" y="31.749998">{{ .Validate.State }}</text>
+    </g>
+
+    <g class="left">
+      <text x="14.811508" y="39.687496">Total tasks:</text>
+      <text x="14.811508" y="44.097221">Pending:</text>
+      <text x="14.811508" y="48.506947">Failed:</text>
+      <text x="14.811508" y="52.916672">Complete:</text>
+      <text x="70.374008" y="39.687496">Total tasks:</text>
+      <text x="70.374008" y="44.097221">Pending:</text>
+      <text x="70.374008" y="48.506947">Failed:</text>
+      <text x="70.374008" y="52.916672">Complete:</text>
+      <text x="125.93651" y="39.687496">Total tasks:</text>
+      <text x="125.93651" y="44.097221">Pending:</text>
+      <text x="125.93651" y="48.506947">Failed:</text>
+      <text x="125.93651" y="52.916672">Complete:</text>
+      <text x="181.49901" y="39.687496">Total tasks:</text>
+      <text x="181.49901" y="44.097221">Pending:</text>
+      <text x="181.49901" y="48.506947">Failed:</text>
+      <text x="181.49901" y="52.916672">Complete:</text>
+      <text x="237.06151" y="39.687496">Total tasks:</text>
+      <text x="237.06151" y="44.097221">Pending:</text>
+      <text x="237.06151" y="48.506947">Failed:</text>
+      <text x="237.06151" y="52.916672">Complete:</text>
+      <text x="292.62399" y="39.687496">Total tasks:</text>
+      <text x="292.62399" y="44.097221">Pending:</text>
+      <text x="292.62399" y="48.506947">Failed:</text>
+      <text x="292.62399" y="52.916672">Complete:</text>
+    </g>
+    <g class="right">
+      <text x="42.160404" y="39.687496">{{ .SCM.NTasks }}</text>
+      <text x="42.160404" y="44.097221">{{ .SCM.NPending }}</text>
+      <text x="42.160404" y="48.506947">{{ .SCM.NFailed }}</text>
+      <text x="42.160404" y="52.916672">{{ .SCM.NComplete }}</text>
+      <text x="97.7229" y="39.687496">{{ .SRPM.NTasks }}</text>
+      <text x="97.7229" y="44.097221">{{ .SRPM.NPending }}</text>
+      <text x="97.7229" y="48.506947">{{ .SRPM.NFailed }}</text>
+      <text x="97.7229" y="52.916672">{{ .SRPM.NComplete }}</text>
+      <text x="153.2854" y="39.687496">{{ .JPB.NTasks }}</text>
+      <text x="153.2854" y="44.097221">{{ .JPB.NPending }}</text>
+      <text x="153.2854" y="48.506947">{{ .JPB.NFailed }}</text>
+      <text x="153.2854" y="52.916672">{{ .JPB.NComplete }}</text>
+      <text x="208.8479" y="39.687496">{{ .Bootstrap.NTasks }}</text>
+      <text x="208.8479" y="44.097221">{{ .Bootstrap.NPending }}</text>
+      <text x="208.8479" y="48.506947">{{ .Bootstrap.NFailed }}</text>
+      <text x="208.8479" y="52.916672">{{ .Bootstrap.NComplete }}</text>
+      <text x="264.4104" y="39.687496">{{ .Rebuild.NTasks }}</text>
+      <text x="264.4104" y="44.097221">{{ .Rebuild.NPending }}</text>
+      <text x="264.4104" y="48.506947">{{ .Rebuild.NFailed }}</text>
+      <text x="264.4104" y="52.916672">{{ .Rebuild.NComplete }}</text>
+      <text x="319.9729" y="39.687496">{{ .Validate.NTasks }}</text>
+      <text x="319.9729" y="44.097221">{{ .Validate.NPending }}</text>
+      <text x="319.9729" y="48.506947">{{ .Validate.NFailed }}</text>
+      <text x="319.9729" y="52.916672">{{ .Validate.NComplete }}</text>
+    </g>
+  </g>
+</svg>
+
 <h2>Workflow details</h2>
+
+{{ with .Workflow }}
 
 <ul>
 {{ range .Tasks }}
 <li>
 Task: <strong>{{.Id}}</strong><br/>
 Handler: {{.Handler}}<br/>
+
+{{ with .Dependencies }}
 Dependencies:<br/>
 <ul>
-{{ range .Dependencies }}
+{{ range . }}
 <li>{{.}}</li>
 {{ end }}
 </ul>
+{{ else }}
+No dependencies<br/>
+{{ end }}
+
+{{ with .Parameters }}
 Parameters:<br/>
 <ul>
-{{ range .Parameters }}
+{{ range . }}
 <li>{{.Name}}: {{.Value}}</li>
 {{ end }}
 </ul>
+{{ else }}
+No parameters<br/>
+{{ end }}
+
+{{ with .Result }}
+
+State: finished<br/>
+Time started: {{.TimeStarted}}<br/>
+Time finished: {{.TimeFinished}}<br/>
+Outcome: {{.Outcome}}<br/>
+Outcome reason: {{.OutcomeReason}}<br/>
+
+{{ with .Artifacts }}
+Artifacts:<br/>
+<ul>
+{{ range . }}
+<li>{{.Type}}: {{.Name}}</li>
+{{ end }}
+</ul>
+{{ else }}
+No artifacts<br/>
+{{ end }}
+
+{{ else }}
+State: not finished<br/>
+{{ end }}
+
 </li>
 {{ end }}
 </ul>
 
+{{ end }}
 
     </main>
     <script src="https://mbi-artifacts.s3.eu-central-1.amazonaws.com/static/bootstrap/js/bootstrap.bundle.min.js"></script>
